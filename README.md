87633b3f149d6d1051eec91ee24edade153b1fda
D. Potikanond <potikanond@gmail.com>
2024-09-16 09:55:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Lab 17 : Enrollment RESTful API
+
+### ป้อนข้อมูลนักศึกษา
+
+รหัส นศ.:
+
+ชื่อ-สกุล :
+
+---
+
+โปรเจคนี้สร้างขึ้นด้วย Next.js (TypeScript)
+หลังจากการ Fork และ Clone repository แล้ว, ให้เปิดโฟลเดอร์ด้วย VSCode และรันคำสั่งต่อไปนี้ใน terminal:
+
+```bash
+npm install
+```
+
+ให้นักศึกษาสร้าง `RESTful API` สำหรับจัดการข้อมูลนักศึกษาอย่างง่ายด้วย Next.js โดยความสามารถที่เพิ่มเติมขึ้นมาจากโค้ดที่พัฒนาในคาบเรียนได้แก่
+
+- การค้นหา นศ. ที่ลงทะเบียนในวิชาที่ต้องการ
+- การถอนการลงทะเบียนในวิชาที่ต้องการ
+
+[คลิกเพื่อดูรายละเอียด](https://o365cmu-my.sharepoint.com/:b:/g/personal/dome_potikanond_cmu_ac_th/EcFkYIYDD8xMuX3iUjjE5vEBWf34fQOyNb5gkzEWNtFSdg?e=1rRUHh)
+
+หลังจาก Deploy ด้วย Vercel แล้ว ให้แปะลิงค์ Deploy และลิงค์ GitHub Repo ของ นศ. ไว้ใน Spreadsheet ด้านล่าง
+
+[คลิกเพื่อเปิด Spreadsheet](https://o365cmu-my.sharepoint.com/:x:/g/personal/dome_potikanond_cmu_ac_th/EfmPM2Wz7OZAiAn23yVwzKwBacdSRIyVOhMjqTMzrHohMg?e=gWuUzE)
---
+++ src/app/api/enrollments/route.ts
@@ -0,0 +1,161 @@
+import { 
+  zEnrollmentGetParam, 
+  zEnrollmentPostBody, 
+  zEnrollmentDeleteBody 
+} from "@lib/schema";
+import { DB, Student } from "@lib/DB";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (request:NextRequest) => {
+  const studentId = request.nextUrl.searchParams.get("studentId");
+  const courseNo = request.nextUrl.searchParams.get("courseNo");
+
+  //validate input
+  const parseResult = zEnrollmentGetParam.safeParse({
+    studentId,
+    courseNo,
+  });
+  if (parseResult.success === false) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: parseResult.error.issues[0].message,
+      },
+      { status: 400 }
+    );
+  }
+
+  //check if user provide one of 'studentId' or 'courseNo'
+  //User must not provide both values, and must not provide nothing
+
+  // return NextResponse.json(
+  //   {
+  //     ok: false,
+  //     message: "Please provide either studentId or courseNo and not both!",
+  //   },
+  //   { status: 400 }
+  // );
+
+  //get all courses enrolled by a student
+  if (studentId) {
+    const courseNoList = [];
+    for (const enroll of DB.enrollments) {
+      if (enroll.studentId === studentId) {
+        courseNoList.push(enroll.courseNo);
+      }
+    }
+
+    const courses = [];
+    for (const courseNo of courseNoList) {
+      const course = DB.courses.find((x) => x.courseNo === courseNo);
+      courses.push(course);
+    }
+
+    return NextResponse.json({
+      ok: true,
+      courses,
+    });
+    //get all students enrolled by a course
+  } else if (courseNo) {
+    const studentIdList = [];
+    for (const enroll of DB.enrollments) {
+      //your code here
+    }
+
+    const students:Student[] = [];
+    //your code here
+
+    return NextResponse.json({
+      ok: true,
+      students,
+    });
+  }
+};
+
+export const POST = async (request:NextRequest) => {
+  const body = await request.json();
+  const parseResult = zEnrollmentPostBody.safeParse(body);
+  if (parseResult.success === false) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: parseResult.error.issues[0].message,
+      },
+      { status: 400 }
+    );
+  }
+
+  const { studentId, courseNo } = body;
+
+  const foundStudent = DB.students.find((x) => x.studentId === studentId);
+  const foundCourse = DB.courses.find((x) => x.courseNo === courseNo);
+  if (!foundStudent || !foundCourse) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Student Id or Course No is not existed",
+      },
+      { status: 400 }
+    );
+  }
+
+  const foundEnroll = DB.enrollments.find(
+    (x) => x.studentId === studentId && x.courseNo === courseNo
+  );
+  if (foundEnroll) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Student already enrolled that course",
+      },
+      { status: 400 }
+    );
+  }
+
+  DB.enrollments.push({
+    studentId,
+    courseNo,
+  });
+
+  return NextResponse.json({
+    ok: true,
+    message: "Student has enrolled course",
+  });
+};
+
+export const DELETE = async (request:NextRequest) => {
+  const body = await request.json();
+
+  //validate body request with zod schema
+  const parseResult = zEnrollmentDeleteBody.safeParse(body);
+  if (parseResult.success === false) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: parseResult.error.issues[0].message,
+      },
+      { status: 400 }
+    );
+  }
+
+  const { studentId, courseNo } = body;
+
+  //check if studentId and courseNo exist on enrollment
+
+  // return NextResponse.json(
+  //   {
+  //     ok: false,
+  //     message: "Enrollment does not exist",
+  //   },
+  //   { status: 404 }
+  // );
+
+  //perform deletion by using splice or array filter
+
+  //if code reach here it means deletion is complete
+  return NextResponse.json({
+    ok: true,
+    message: "Enrollment has been deleted",
+  });
+};
+
